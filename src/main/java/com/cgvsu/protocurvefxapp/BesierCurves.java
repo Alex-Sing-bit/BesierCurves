6ad084a323e56b4bb5a7a55952cee7f4eb408667
Sing Alex <dexp51477@gmail.com>
2023-10-04 19:16:50 +0300

    Add base example programm

---
+++ src/main/java/com/cgvsu/protocurvefxapp/BesierCurves.java
@@ -0,0 +1,12 @@
+package com.cgvsu.protocurvefxapp;
+
+import javafx.geometry.Point2D;
+import javafx.scene.canvas.GraphicsContext;
+
+import java.util.ArrayList;
+
+public class BesierCurves {
+    public static void drawBesierCurve(final GraphicsContext graphicsContext, ArrayList<Point2D> points) {
+        
+    }
+}
